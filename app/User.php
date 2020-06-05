607d5a6d8f08d82a544cce1399f1f3ddadf3c37f
adityakunhare <honey.kunhare@gmail.com>
2020-06-05 18:57:19 +0530

    update2

--- app/User.php
+++ app/User.php
@@ -7,19 +7,10 @@ use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-<<<<<<< HEAD
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
-=======
->>>>>>> 105331cc5305c0b4b4b0d96e51eb59250e1c6c94
 
 class User extends Authenticatable
 {
     use HasApiTokens, Notifiable;
-<<<<<<< HEAD
-    use Notifiable;
-=======
->>>>>>> 105331cc5305c0b4b4b0d96e51eb59250e1c6c94
 
     /**
      * The attributes that are mass assignable.
